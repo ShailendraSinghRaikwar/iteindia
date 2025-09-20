24a676e7b53ea3de387a5838807ea2e6a15f212e
Shailendra Singh <shailendrasinghraikwar@gmail.com>
2025-09-20 15:44:50 +0530

    Hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { FaArrowRight } from "react-icons/fa6";
+import { FaArrowRight, FaCheck } from "react-icons/fa6";
 
 export default function Hero() {
   return (
@@ -10,10 +10,30 @@ export default function Hero() {
         <h1 className="leading-loose text-3xl md:text-7xl font-bold mb-8">
           Welcome To <br /> ITE - Institute for Technical <br /> Education
         </h1>
-        <p className="max-w-2xl mx-auto text-lg md:text-xl mb-6">
-          We deliver web & mobile apps, cloud transformation, training and
-          end-to-end IT services that accelerate growth.
-        </p>
+
+        <div className="flex justify-center gap-10  m-5">
+          <h3 className="flex items-center gap-2">
+            <FaCheck />
+            Redhat Courses
+          </h3>
+          <h3 className="flex items-center gap-2">
+            <FaCheck />
+            AWS Courses
+          </h3>
+          <h3 className="flex items-center gap-2">
+            <FaCheck />
+            Cyber Security Courses
+          </h3>
+          <h3 className="flex items-center gap-2">
+            <FaCheck />
+            DepOps Courses
+          </h3>
+        </div>
+
+        {/* <p className="max-w-2xl mx-auto text-sm md:text-xl mb-6">
+          <span>Redhat Courses</span> <span>AWS Courses</span>
+          <span>Cyber Security Courses</span> <span>DevOPs Courses</span>
+        </p> */}
         <div className="flex justify-center gap-4">
           <a
             href="#services"
